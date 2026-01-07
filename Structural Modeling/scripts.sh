2b6e27528bb428cbcc1ec581f3a43b77f9a27406
Alan Samuel Gomez Paz <gomezpazalansamuel@gmail.com>
2026-01-07 16:44:59 -0600

    Terminando carpetas par atrabajar

--- Structural Modeling/scripts.sh
+++ Structural Modeling/scripts.sh
@@ -1,54 +1,54 @@
-mkdir 3_Half_Subtractor
-cp -r ./1_Half_Adder/Pictures 3_*
-mkdir 4_Full_Subtractor
-cp -r ./1_Half_Adder/Pictures 4_*
-mkdir 5_Ripple_Carry_Adder
-cp -r ./1_Half_Adder/Pictures 5_*
-mkdir 6_4-bit_Adder_Subtractor
-cp -r ./1_Half_Adder/Pictures 6_*
-mkdir 7_Binary_arrowright_Gray_Code_Converter
-cp -r ./1_Half_Adder/Pictures 7_*
-mkdir 8_Gray_arrowright_Binary_Code_Converter
-cp -r ./1_Half_Adder/Pictures 8_*
-mkdir 9_Multiplexer
-cp -r ./1_Half_Adder/Pictures 9_*
+mkdir 03_Half_Subtractor
+cp -r ./01_Half_Adder/Pictures 03_*
+mkdir 04_Full_Subtractor
+cp -r ./01_Half_Adder/Pictures 04_*
+mkdir 05_Ripple_Carry_Adder
+cp -r ./01_Half_Adder/Pictures 05_*
+mkdir 06_4-bit_Adder_Subtractor
+cp -r ./01_Half_Adder/Pictures 06_*
+mkdir 07_Binary_arrowright_Gray_Code_Converter
+cp -r ./01_Half_Adder/Pictures 07_*
+mkdir 08_Gray_arrowright_Binary_Code_Converter
+cp -r ./01_Half_Adder/Pictures 08_*
+mkdir 09_Multiplexer
+cp -r ./01_Half_Adder/Pictures 09_*
 mkdir 10_Demultiplexer
-cp -r ./1_Half_Adder/Pictures 10_*
+cp -r ./01_Half_Adder/Pictures 10_*
 mkdir 11_Encoder
-cp -r ./1_Half_Adder/Pictures 11_*
+cp -r ./01_Half_Adder/Pictures 11_*
 mkdir 12_Priority_Encoder
-cp -r ./1_Half_Adder/Pictures 12_*
+cp -r ./01_Half_Adder/Pictures 12_*
 mkdir 13_Decoder
-cp -r ./1_Half_Adder/Pictures 13_*
+cp -r ./01_Half_Adder/Pictures 13_*
 mkdir 14_Comparator
-cp -r ./1_Half_Adder/Pictures 14_*
+cp -r ./01_Half_Adder/Pictures 14_*
 mkdir 15_Array_Multiplier
-cp -r ./1_Half_Adder/Pictures 15_*
+cp -r ./01_Half_Adder/Pictures 15_*
 mkdir 16_Booths_Multiplier
-cp -r ./1_Half_Adder/Pictures 16_*
+cp -r ./01_Half_Adder/Pictures 16_*
 mkdir 17_D_Flip-Flop_Asynchronous_Reset
-cp -r ./1_Half_Adder/Pictures 17_*
+cp -r ./01_Half_Adder/Pictures 17_*
 mkdir 18_D_Flip-Flop_Synchronous_Reset
-cp -r ./1_Half_Adder/Pictures 18_*
+cp -r ./01_Half_Adder/Pictures 18_*
 mkdir 19_SR_Flip-Flop
-cp -r ./1_Half_Adder/Pictures 19_*
+cp -r ./01_Half_Adder/Pictures 19_*
 mkdir 20_JK_Flip-Flop
-cp -r ./1_Half_Adder/Pictures 20_*
+cp -r ./01_Half_Adder/Pictures 20_*
 mkdir 21_T_Flip-Flop
-cp -r ./1_Half_Adder/Pictures 21_*
+cp -r ./01_Half_Adder/Pictures 21_*
 mkdir 22_Universal_Shift_Register
-cp -r ./1_Half_Adder/Pictures 22_*
+cp -r ./01_Half_Adder/Pictures 22_*
 mkdir 23_Linear_Feedback_Shift_Register
-cp -r ./1_Half_Adder/Pictures 23_*
+cp -r ./01_Half_Adder/Pictures 23_*
 mkdir 24_Asynchronous_Counter
-cp -r ./1_Half_Adder/Pictures 24_*
+cp -r ./01_Half_Adder/Pictures 24_*
 mkdir 25_Synchronous_Counter
-cp -r ./1_Half_Adder/Pictures 25_*
+cp -r ./01_Half_Adder/Pictures 25_*
 mkdir 26_Mealy_Sequence_Detector
-cp -r ./1_Half_Adder/Pictures 26_*
+cp -r ./01_Half_Adder/Pictures 26_*
 mkdir 27_Moore_Sequence_Detector
-cp -r ./1_Half_Adder/Pictures 27_*
+cp -r ./01_Half_Adder/Pictures 27_*
 mkdir 28_Synchronous_FIFO
-cp -r ./1_Half_Adder/Pictures 28_*
+cp -r ./01_Half_Adder/Pictures 28_*
 mkdir 29_Asynchronous_FIFO
-cp -r ./1_Half_Adder/Pictures 29_*
+cp -r ./01_Half_Adder/Pictures 29_*
